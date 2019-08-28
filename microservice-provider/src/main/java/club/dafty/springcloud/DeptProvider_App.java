f3ba81504a977350cfce1c80f8977bed0a8e9575
DaftLee <leechengchao@126.com>
2019-08-29 01:01:58 +0800

    Eureka provider完成

--- microservice-provider/src/main/java/club/dafty/springcloud/DeptProvider_App.java
+++ microservice-provider/src/main/java/club/dafty/springcloud/DeptProvider_App.java
@@ -2,8 +2,10 @@ package club.dafty.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptProvider_App {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider_App.class,args);
